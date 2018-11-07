057c53fb2b6abd686539d58481222b8665890938
Fiona Lim Sze Peh <fionalim@navig8group.com>
2018-11-07 15:18:28 +0800

    Add new component "Header" to CoreModule

--- ClientApp/src/app/core/core.module.ts
+++ ClientApp/src/app/core/core.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from 'src/app/core/components/header/header.component';
 import { PageNotFoundComponent } from 'src/app/core/pages/page-not-found/page-not-found.component';
 
 @NgModule({
   imports: [],
-  declarations: [PageNotFoundComponent],
-  exports: [PageNotFoundComponent]
+  declarations: [PageNotFoundComponent, HeaderComponent],
+  exports: [PageNotFoundComponent, HeaderComponent]
 })
 export class CoreModule {}
